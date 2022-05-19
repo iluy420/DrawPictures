bd9e8dddabb3c1625394b420054e01b607de8b05
iluy.marckov@mail.ru <iluy.marckov@mail.ru>
2022-05-19 22:28:25 +0300

    Базовый класс модели представления

--- DrawPictures/Views/Picture.xaml.cs
+++ DrawPictures/Views/Picture.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,39 +21,51 @@ namespace DrawPictures
         public static RoutedCommand MyCommand = new RoutedCommand();
         public static RoutedCommand MyCommand1 = new RoutedCommand();
         public System.Windows.Ink.StrokeCollection _added;
-        public System.Windows.Ink.StrokeCollection _removed;
-        private bool handle = true;
+        public static System.Windows.Ink.StrokeCollection _removed;
+        //private bool handle = true;
         public Picture()
         {
             InitializeComponent();
             MyCommand.InputGestures.Add(new KeyGesture(Key.Z, ModifierKeys.Control));
             MyCommand1.InputGestures.Add(new KeyGesture(Key.Y, ModifierKeys.Control));
-            ink.Strokes.StrokesChanged += Strokes_StrokesChanged;
+            //ink.Strokes.StrokesChanged += Strokes_StrokesChanged;
         }
-        private void Strokes_StrokesChanged(object sender, System.Windows.Ink.StrokeCollectionChangedEventArgs e)
-        {
-            //if (handle)
-            //{
-            //    if(_added != null)
-            //    {
-            //        _added.Add(e.Added);
-            //    }
-            //    else
-            //    {
-                    _added = e.Added;
-                    _removed = e.Removed;
-                //}
+        //private void Strokes_StrokesChanged(object sender, System.Windows.Ink.StrokeCollectionChangedEventArgs e)
+        //{
+        //    //if (handle)
+        //    //{
+        //    if (_removed == null)
+        //    {
+        //        _removed = e.Removed;
                 
+        //    }
+        //    else
+        //    {
+        //        _removed.Add(e.Removed);
+        //    }
+               
+        //    //}
 
-            //}
-        }
+
+        //    //}
+        //}
         private void Undo_Click(object sender, RoutedEventArgs e)
         {
             if (ink.Strokes.Count > 0)
             {
-                //_removed.Add(ink.Strokes[ink.Strokes.Count - 1]);
+                int gg = ink.Strokes.Count - 1;
+                Stroke strokes = ink.Strokes[gg].Clone();
+                _removed.Add(strokes);
                 ink.Strokes.RemoveAt(ink.Strokes.Count - 1);
-                
+            }
+        }
+        private void Redo_Click(object sender, RoutedEventArgs e)
+        {
+            if (ink.Strokes.Count > 0)
+            {
+                _removed.Add(ink.Strokes[ink.Strokes.Count - 1]);
+                ink.Strokes.Add(_removed);
+
             }
         }
 
---
+++ DrawPictures/ViewsModels/Base/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DrawPictures.ViewsModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged 
+    {
+        //интерфейс способный уведомлять о том что внутри нашего объекта изменилось какое-то свойство
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+    }
+}
