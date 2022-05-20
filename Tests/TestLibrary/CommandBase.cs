8d69f2d3288bcb056ef348c4c2e2925c088b3d7d
iluy.marckov@mail.ru <iluy.marckov@mail.ru>
2022-05-20 09:51:02 +0300

    Тест использования элементов WPF в библиотеке

---
+++ Tests/TestLibrary/CommandBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TestLibrary
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);//функция. Если false то коману выполнить нельзя, true можно
+
+        public abstract void Execute(object parameter); //основная логика команды
+
+    }
+}
