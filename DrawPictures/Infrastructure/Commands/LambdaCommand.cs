350f51632d1e44eec75dbd4e53c2b718a018bd19
iluy.marckov@mail.ru <iluy.marckov@mail.ru>
2022-05-20 10:01:52 +0300

    Главный класс команды

---
+++ DrawPictures/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DrawPictures.Infrastructure.Commands.Base;
+
+namespace DrawPictures.Infrastructure.Commands
+{
+    internal class LambdaCommand:CommandBase
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object,bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _execute(parameter);
+    }
+}
