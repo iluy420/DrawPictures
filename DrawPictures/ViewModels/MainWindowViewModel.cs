fd7ff0478d42af383e43ecbe7f58faf42153446c
iluy.marckov@mail.ru <iluy.marckov@mail.ru>
2022-05-26 13:51:41 +0300

    попЫтка намисать событие для швиндус клоузен

--- DrawPictures/ViewModels/MainWindowViewModel.cs
+++ DrawPictures/ViewModels/MainWindowViewModel.cs
@@ -176,5 +176,64 @@ namespace DrawPictures.ViewModels
 
             #endregion
         }
+
+
+        //void CloseWindow()
+        //{
+        //    Close?.Invoke();
+        //}
+        //public Action Close { get; set; }
+
+        //public bool CanClose()
+        //{
+        //    return false;
+        //}
     }
+
+    //interface ICloseWindows
+    //{
+    //    Action Close { get; set; }
+    //    bool CanClose();
+
+    //}
+
+    //public class WindowCloser
+    //{
+    //    public static bool GetEnableWindowClosing(DependencyObject obj)
+    //    {
+    //        return (bool)obj.GetValue(EnableWindowClosingProperty);
+    //    }
+
+    //    public static void SetEnableWindowClosing(DependencyObject obj, bool value)
+    //    {
+    //        obj.SetValue(EnableWindowClosingProperty,value);
+    //    }
+
+        
+    //}
+    //public static readonly DependencyProperty EnableWindowClosingProperty =
+    //    DependencyProperty.RegisterAttached("EnableWindowClosing", typeof(bool), typeof(WindowCloser),
+    //        new PropertyMetadata(false, OnEnableWindowClosingChenged));
+
+    //private static void OnEnableWindowClosingChenged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    //{
+    //    if (d is Window window)
+    //    {
+    //        window.Loaded += (s, e) =>
+    //        {
+    //            if (window.DataContext is ICloseWindows vm)
+    //            {
+    //                vm.Close += () =>
+    //                {
+    //                    window.Close();
+    //                };
+    //                window.Closing += (s, e) =>
+    //                {
+    //                    e.Cancel = !vm.CanClose();
+    //                };
+
+    //            }
+    //        };
+    //    }
+    //}
 }
